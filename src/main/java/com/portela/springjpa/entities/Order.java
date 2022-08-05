39007d9c4d55d71a8f57d59634b5b2ef7c060628
Gabsonrogeer <gabsonwona@gmail.com>
2022-08-05 14:07:30 -0300

    Order StatusEnum

--- src/main/java/com/portela/springjpa/entities/Order.java
+++ src/main/java/com/portela/springjpa/entities/Order.java
@@ -2,6 +2,7 @@ package com.portela.springjpa.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.portela.springjpa.entities.enums.OrderStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.persistence.*;
@@ -17,8 +18,12 @@ public class Order implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+    @JsonFormat(shape = JsonFormat.Shape.STRING,
+            pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",
+            timezone = "GMT")
     private Instant moment;
+
+    private Integer orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -29,9 +34,11 @@ public class Order implements Serializable {
 
     public Order(Long id,
                  Instant moment,
+                 OrderStatus orderStatus,
                  User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -51,6 +58,15 @@ public class Order implements Serializable {
         this.moment = moment;
     }
 
+    public OrderStatus getOrderStatus(){
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus){
+        if (orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
     public User getClient() {
         return client;
     }
