8ff2f4787cbe3c0e18c907eb0f34a4134b020c1e
Gabsonrogeer <gabsonwona@gmail.com>
2022-08-05 18:36:13 -0300

    orderitem

---
+++ src/main/java/com/portela/springjpa/entities/OrderItem.java
@@ -0,0 +1,66 @@
+package com.portela.springjpa.entities;
+
+import com.portela.springjpa.entities.pk.OrderItemPK;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+
+    @EmbeddedId
+    private OrderItemPK id;
+    private Integer quantity;
+    private Double price;
+
+    public OrderItem(){}
+
+    public OrderItem(Order order,
+                     Product product,
+                     Integer quantity,
+                     Double price) {
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Order gerOrder(){
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order){
+        id.setOrder(order);
+    }
+
+    public Product gerProduct(){
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product){
+        id.setProduct(product);
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+
+}
