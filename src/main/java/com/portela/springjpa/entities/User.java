a30c5b835ebd0ee30338075fbeda3d66eafdba18
Gabsonrogeer <gabsonwona@gmail.com>
2022-08-05 11:01:42 -0300

    H2 database, test profile, JPA

--- src/main/java/com/portela/springjpa/entities/User.java
+++ src/main/java/com/portela/springjpa/entities/User.java
@@ -1,14 +1,15 @@
 package com.portela.springjpa.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
+@Table(name = "users")
 public class User implements Serializable {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
